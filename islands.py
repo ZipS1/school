d57eedead2cf4e1819625c52636a822a389b9d5d
ZipS <czwetkow.serega@yandex.ru>
2020-11-12 11:18:58 +0300

    clear all extra progs

--- islands.py
+++ islands.py
@@ -5,21 +5,30 @@ WIDTH = 4
 matrix = matrices.genMx(WIDTH, HEIGHT, 0, 1, True)
 matrices.printMx(matrix)
 
-island_count = 0
 color = 1
+
 area = []
 passed_pts = []
+island_count = 0
+
 
 for ix in range(WIDTH):
     for iy in range(HEIGHT):
         if matrix[iy][ix] == 1 and (iy, ix) not in passed_pts:
-            start_point = (iy, ix)
-            passed_pts.append(start_point)
-            area.append(start_point)
+            area = [(iy, ix)] 
             while area != []:
-                y, x = area[0]
+                point = area[0]   # !!!! ВОТ ОНА ПОГАНКА (↓) !!!! Цикл перебирал только точки вокруг стартовой
+                # point = (iy, ix)
+                y = point[0]
+                x = point[1]
+                # !!! И ЕЩЕ ОДНА !!! Проверка начальной точки была проведена выше, надобности проверять нет
+                # if (y, x) in passed_pts: 
+                #     continue
+
+
+                # А вот проверка точек которые мы в массив добавляем (↓) критически необходима
                 if x - 1 >= 0:
-                    if matrix[y][x - 1] == color and (y, x - 1) not in passed_pts:
+                    if matrix[y][x - 1] == color and (y, x - 1) not in passed_pts:   
                         area.append((y, x - 1))
                         passed_pts.append((y, x - 1))
                 if x + 1 <= (WIDTH - 1):
@@ -35,7 +44,9 @@ for ix in range(WIDTH):
                         area.append((y + 1, x))
                         passed_pts.append((y + 1, x))
                 area.pop(0)
-            area = []
             island_count += 1
-
+            # Это тоже лишнее, т.к условием выхода из цикла уже является пустой список
+            # area = []
+            
 print("Островов:", island_count)
+
